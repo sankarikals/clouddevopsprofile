daf32e1254c92c110caffb505e353afbbef5f5b2
sankarikals <81800800+sankarikals@users.noreply.github.com>
2026-03-30 16:06:17 +0530

    Update backend.tf

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,3 @@
-backend "s3" {
-  bucket         = "terraform-state-terraformcicd"
-  key            = "staticwebsite/terraform.tfstate"
-  region         = "ap-south-1"
-  #dynamodb_table = "terraform-lock"
+terraform {
+  backend "s3" {}
 }
